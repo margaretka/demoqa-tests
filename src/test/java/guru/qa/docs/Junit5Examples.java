272d21aa5ad3b14f89921ca84c02d5f733c3e2f0
mak <margarita.kalinina24@yandex.ru>
2021-09-11 18:34:44 +0500

    rename, docs

---
+++ src/test/java/guru/qa/docs/Junit5Examples.java
@@ -0,0 +1,36 @@
+package guru.qa.docs;
+
+import org.junit.jupiter.api.*;
+
+public class Junit5Examples {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before all\n");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("After all\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before test\n");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("I love you QA.GURU\n");
+    }
+
+    @Test
+    void sevondTest() {
+        System.out.println("I love you QA.GURU, 2\n");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After test\n");
+    }
+}
