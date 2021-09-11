fd009bc5409f78fec8b982ae70ae292555e92286
mak <margarita.kalinina24@yandex.ru>
2021-09-11 19:12:05 +0500

    add TextBoxTests

---
+++ src/test/java/guru/qa/tests/TextBoxTests.java
@@ -0,0 +1,32 @@
+package guru.qa.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alex@google.com");
+        $("#currentAddress").setValue("street 1");
+        $("#permanentAddress").setValue("street 2");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@google.com"));
+        $("#output #currentAddress").shouldHave(text("street 1"));
+        $("#output #permanentAddress").shouldHave(text("street 2"));
+    }
+}
